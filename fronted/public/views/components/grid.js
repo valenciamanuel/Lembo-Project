f99297a972e36a2616c8467655ef278553c7430b
Jose Obregon <joseobregon270@gmail.com>
2025-04-06 15:19:42 -0500

    FETCH 100% AGARRA
    FECTH 100%

--- fronted/public/views/components/grid.js
+++ fronted/public/views/components/grid.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
-    fetch("/frontend/public/views/components/grid.html")
+    fetch("/fronted/public/views/components/grid.html")
     .then(response => response.text())
     .then(data =>{
         document.querySelector(".grid").innerHTML = data;
     })
-    .catch(error => console.error("Error cargando footer:", error));
+    .catch(error => console.error("Error al cargar los grids:", error));
 });
